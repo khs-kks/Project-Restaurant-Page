62cb8ac3b61f6bfa5a3c00860323d8d3ccce8316
khs <ksalumiev@gmail.com>
2022-12-19 18:29:47 +0100

    Finish rendering the homepage module

--- src/pageload.js
+++ src/pageload.js
@@ -5,6 +5,7 @@ export default function loadHomepage() {
   const footer = document.createElement("div");
 
   header.classList.add("header");
+
   //   POPULATE HEADER
 
   const h1 = document.createElement("h1");
@@ -32,4 +33,50 @@ export default function loadHomepage() {
   header.appendChild(tabs);
 
   content.appendChild(header);
+
+  //   POPULATE MAIN
+
+  main.classList.add("main");
+
+  const mainContent = document.createElement("div");
+  mainContent.classList.add("main-content");
+
+  const bread = document.createElement("div");
+  bread.textContent = "Best bread in your town";
+
+  const since = document.createElement("div");
+  since.textContent = "Made with passion since 1899";
+
+  const img = document.createElement("img");
+  img.setAttribute("src", "./pexels-malidate-van-784632.jpg");
+  img.setAttribute("alt", "Our bakery");
+  img.setAttribute("height", "330px");
+  img.setAttribute("width", "330px");
+  img.classList.add("img-main");
+
+  const order = document.createElement("div");
+  order.textContent = "Order Online or visit us!";
+
+  mainContent.appendChild(bread);
+  mainContent.appendChild(since);
+  mainContent.appendChild(img);
+  mainContent.appendChild(order);
+
+  main.appendChild(mainContent);
+  content.appendChild(main);
+
+  //   POPULATE FOOTER
+
+  footer.classList.add("footer");
+  footer.textContent = "Copyleft 2022";
+
+  const githubLink = document.createElement("a");
+  githubLink.setAttribute(
+    "href",
+    "https://github.com/khs-kks/Project-Restaurant-Page"
+  );
+  githubLink.textContent = " KHS";
+
+  footer.appendChild(githubLink);
+  content.appendChild(footer);
 }
