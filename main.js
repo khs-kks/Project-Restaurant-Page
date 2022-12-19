d575fc0925a2855d10214bddc1076b1311a29663
khs <ksalumiev@gmail.com>
2022-12-19 13:53:02 +0100

    Setup the env

---
+++ main.js
@@ -0,0 +1,12 @@
+/* eslint-disable spaced-comment */
+/******/ (() => {
+  // webpackBootstrap
+  var __webpack_exports__ = {};
+  /*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+  /* eslint-disable no-alert */
+
+  /******/
+})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcHJvamVjdC1yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLyogZXNsaW50LWRpc2FibGUgbm8tYWxlcnQgKi9cblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
